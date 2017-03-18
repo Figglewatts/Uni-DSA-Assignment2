a7d861f1a3cbde86713e70cfd58e1af9b1c84769
Sam Gibson <me@figglewatts.co.uk>
2017-03-18 11:31:01 +0000

    Begun Question 1 implementation

---
+++ DSAAssignment2/src/dsaassignment2/question1/Question1Main.java
@@ -0,0 +1,10 @@
+package dsaassignment2.question1;
+
+/**
+ * @author Sam Gibson (100115871) <sam.gibson@uea.ac.uk>
+ */
+public class Question1Main {
+    public static void main (String[] args) {
+        
+    }
+}
