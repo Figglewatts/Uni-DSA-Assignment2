07fba9964c17cbf94f51f5c17527e637ed909a8c
Sam Gibson <me@figglewatts.co.uk>
2017-03-18 11:52:42 +0000

    Add matrix class and algorithm method scaffolding

---
+++ DSAAssignment2/src/dsaassignment2/question1/Matrix.java
@@ -0,0 +1,40 @@
+package dsaassignment2.question1;
+
+/**
+ *
+ * @author Sam Gibson (100115871) <sam.gibson@uea.ac.uk>
+ */
+public class Matrix {
+    private final int[][] values;
+    private final int width;
+    private final int height;
+    
+    public Matrix(int h, int w) {
+        this.values = new int[h][w];
+        this.width = w;
+        this.height = h;
+    }
+    
+    public Matrix(int[][] values) {
+        this.values = values;
+        this.height = values.length;
+        this.width = values[0].length;
+    }
+    
+    public static Matrix defaultMatrix() {
+        int[][] values = {
+            { 1, 3, 7, 8, 8, 9, 12 },
+            { 2, 4, 8, 9, 10, 30, 38 },
+            { 4, 5, 10, 20, 29, 50, 60 },
+            { 8, 10, 11, 30, 50, 60, 61 },
+            { 11, 12, 40, 80, 90, 100, 111 },
+            { 13, 15, 50, 100, 110, 112, 120 },
+            { 22, 27, 61, 112, 119, 138, 153 }
+        };
+        return new Matrix(values);
+    }
+    
+    public int get(int y, int x) {
+        return this.values[y][x];
+    }
+}
--- DSAAssignment2/src/dsaassignment2/question1/Question1Main.java
+++ DSAAssignment2/src/dsaassignment2/question1/Question1Main.java
@@ -7,4 +7,20 @@ public class Question1Main {
     public static void main (String[] args) {
         
     }
+    
+    public static boolean findElementD(Matrix a, int n, int p) {
+        return false;
+    }
+    
+    public static boolean findElementD1(Matrix a, int n, int p) {
+        return false;
+    }
+    
+    public static boolean findElementD2(Matrix a, int n, int p) {
+        return false;
+    }
+    
+    private static boolean binarySearch(int rowNum, int p, int n, Matrix a) {
+        return false;
+    }
 }
